0ec0523caaa198a4ab6e60b4de19dcd0b01b875c
JuanDiazDevPO <jediazv@unbosque.edu.co>
2026-04-06 15:54:13 -0500

    adding greeting message

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,8 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SessionService } from '../session.service';
+import { Usuario } from '../auth.models';
 
 @Component({
   standalone: true,
   selector: 'app-dashboard',
-  template: '<h2>Dashboard</h2>'
+  templateUrl: './dashboard.component.html',
+  styleUrl: './dashboard.component.css'
 })
-export class DashboardComponent {}
+export class DashboardComponent implements OnInit {
+  user: Usuario | null = null;
+
+  constructor(private session: SessionService) {}
+
+  ngOnInit() {
+    this.user = this.session.getUser();
+  }
+}
