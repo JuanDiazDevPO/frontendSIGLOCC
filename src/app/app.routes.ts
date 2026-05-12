74a0cadb9635e830b053588bfb5c9cdbe11c9085
JuanDiazDevPO <jediazv@unbosque.edu.co>
2026-05-12 13:56:21 -0500

    singout implementation

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,17 @@
 import { Routes } from '@angular/router';
+import { authGuard, guestGuard } from './auth.guard';
 
 export const routes: Routes = [
   {
     path: 'login',
+    canActivate: [guestGuard],
     loadComponent: () =>
       import('./login/login.component')
         .then(m => m.LoginComponent)
   },
   {
     path: 'dashboard',
+    canActivate: [authGuard],
     loadComponent: () =>
       import('./dashboard/dashboard.component')
         .then(m => m.DashboardComponent)
@@ -27,6 +30,7 @@ export const routes: Routes = [
   },
   {
     path: 'anticipos/crear',
+    canActivate: [authGuard],
     loadComponent: () =>
       import('./crear-anticipo/crear-anticipo.component')
         .then(m => m.CrearAnticipoComponent)
